c94e7040bd885f821848f2e334965cf510101555
Hossein Moein <myseajune@aol.com>
2023-09-05 13:56:35 -0400

    Reorganized the test

--- test/thrpool_tester.cc
+++ test/thrpool_tester.cc
@@ -3,18 +3,21 @@
 
 #include <ThreadPool/ThreadPool.h>
 
+#include <cassert>
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <numeric>
 #include <string>
+#include <vector>
 
 using namespace hmthrp;
 
 // ----------------------------------------------------------------------------
 
-static const std::size_t    THREAD_COUNT = 5;
-static std::mutex           GLOCK { };
+static constexpr std::size_t    THREAD_COUNT = 5;
+static std::mutex               GLOCK { };
 
 // ----------------------------------------------------------------------------
 
@@ -59,7 +62,9 @@ my_func(std::size_t i, double d, const std::string &s)  {
 
 // ----------------------------------------------------------------------------
 
-int main (int, char *[])  {
+static void haphazard()  {
+
+    std::cout << "Running haphazard() ..." << std::endl;
 
     ThreadPool  thr_pool (THREAD_COUNT, true, 10);
 
@@ -163,6 +168,80 @@ int main (int, char *[])  {
                   << "available: " << thr_pool.available_threads()
                   << std::endl;
     }
+
+    return;
+}
+
+// ----------------------------------------------------------------------------
+
+static void repeating_thread_id()  {
+
+    std::cout << "Running repeating_thread_id() ..." << std::endl;
+
+    ThreadPool  thr_pool { THREAD_COUNT };
+
+    for (std::size_t i = 0; i < 50; ++i)
+        thr_pool.dispatch(
+            false,
+            [](std::size_t i) -> void {
+                const std::lock_guard<std::mutex>   lock { GLOCK };
+
+                std::cout << "From Lambda: " << i
+                          << " -- " << std::this_thread::get_id()
+                          << std::endl;
+            },
+            i);
+
+    return;
+}
+
+// ----------------------------------------------------------------------------
+
+static void parallel_accumulate()  {
+
+    std::cout << "Running parallel_accumulate() ..." << std::endl;
+
+    constexpr std::size_t       n { 10003 };
+    constexpr std::size_t       the_sum { (n * (n + 1)) / 2 };
+    std::vector<std::size_t>    vec (n);
+
+    std::iota(vec.begin(), vec.end(), 1);
+
+    constexpr std::size_t                   block_size { n / THREAD_COUNT };
+    std::vector<std::future<std::size_t>>   futs;
+    auto                                    block_start = vec.begin();
+    ThreadPool                              thr_pool { THREAD_COUNT };
+
+    futs.reserve(THREAD_COUNT - 1);
+    for (std::size_t i = 0; i < (THREAD_COUNT - 1); ++i)  {
+        const auto  block_end { block_start + block_size };
+
+        futs.push_back(
+            thr_pool.dispatch(
+                false,
+                std::accumulate<decltype(block_start), std::size_t>,
+                block_start, block_end, 0));
+        block_start = block_end;
+    }
+
+    // Last result
+    //
+    std::size_t result { std::accumulate(block_start, vec.end(), 0UL) };
+
+    for (std::size_t i = 0; i < futs.size(); ++i)
+        result += futs[i].get();
+
+    assert(result == the_sum);
+    return;
+}
+
+// ----------------------------------------------------------------------------
+
+int main (int, char *[])  {
+
+    haphazard();
+    repeating_thread_id();
+    parallel_accumulate();
 
     return (EXIT_SUCCESS);
 }
